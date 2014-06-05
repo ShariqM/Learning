44f02932f3a80de04cc9c41c5d401fbf0a90519a
Shariq Mobin <Shariq.Mobin@gmail.com>
2014-06-05 00:03:21 -0700

    chain test PICKED

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ ENVIRON = []                # Ignore, initialized by the runner
 MAZE    = 'maze.mz'         # See files in maze_files/ dir
 #MAZE    = 'maze_3d.mz'     # See files in maze_files/ dir
 STEPS   = 1000              # Number of time steps to run
-RUNS    = 100               # Number of runs
+RUNS    = 500               # Number of runs
 SERIAL  = False
 
 
@@ -170,11 +170,12 @@ def init_strats():
                  #COLORS['green'], PLUS=0, EXPLORER=False))
 
     #for i in xrange(110,120,1):
-    for i in xrange(70,130,2):
-        im = ChineseRProcess(ENVIRON[0], THETA, ALPHA, False, True)
-        arr.append(DyStrat(ENVIRON[0], im,
-                            PigVIStrat(ENVIRON[0], im, PLUS=0, EXPLORER=False),
-                            BossSAStrat(ENVIRON[0], im, -1), i))
+    for r in range(30):
+        for i in (120,124):
+            im = ChineseRProcess(ENVIRON[0], THETA, ALPHA, False, True)
+            arr.append(DyStrat(ENVIRON[0], im,
+                                PigVIStrat(ENVIRON[0], im, PLUS=0, EXPLORER=False),
+                                BossSAStrat(ENVIRON[0], im, -1), i))
 
 
 

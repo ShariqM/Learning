8aa168afa55973737054e82ac26e2fb372019e1f
Shariq Mobin <Shariq.Mobin@gmail.com>
2014-06-05 10:52:41 -0700

    DP test

--- config.py
+++ config.py
@@ -53,8 +53,8 @@ ENVIRON = []                # Ignore, initialized by the runner
 #MAZE    = 'maze_s30.mz'    # See files in maze_files/ dir
 MAZE    = 'maze.mz'         # See files in maze_files/ dir
 #MAZE    = 'maze_3d.mz'     # See files in maze_files/ dir
-STEPS   = 300               # Number of time steps to run
-RUNS    = 1                 # Number of runs
+STEPS   = 3000              # Number of time steps to run
+RUNS    = 50                # Number of runs
 SERIAL  = False
 
 

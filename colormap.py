26c7d9566723f5dc8bdc1cff4d98f4c79bfd7721
Shariq Mobin <Shariq.Mobin@gmail.com>
2014-07-01 18:10:10 -0700

    remove dead files

--- colormap.py
+++
@@ -1,8 +0,0 @@
-
-from pylab import *
-my_cmap = get_cmap('autumn')
-pcolor(rand(10,10),cmap=my_cmap)
-colorbar()
-print 'foo'
-
-matplotlib.pyplot.show()
--- err.py
+++
@@ -1,8 +0,0 @@
-
-
-
-try:
-    a = 1 / 0
-except Exception as e:
-    print dir(e)
-    print ecatch

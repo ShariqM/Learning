c102566b942edae85945aeb7eb1e3153e21b61b4
Shariq Mobin <Shariq.Mobin@gmail.com>
2014-07-14 16:15:35 -0700

    settings eb test

---
+++ settings/settings_eb_test.py
@@ -0,0 +1,34 @@
+from strats.randomstrat import *
+from strats.unembodiedstrat import *
+from strats.piggreedystrat import *
+from strats.pigvistrat import *
+from strats.ltastrat import *
+from strats.ltavistrat import *
+from strats.cbstrat import *
+from strats.bossstrat import *
+from strats.dystrat import *
+from strats.chainstrat import *
+
+from imodels.chinese import *
+from imodels.dirichlet import *
+
+from config import *
+
+# Run parameters
+SS      = 0
+MAZE    = 'maze_complex_s10b.mz'    # See files in maze_files/ dir
+STEPS   = 3000              # Number of time steps to run
+RUNS    = 20                # Number of runs
+
+def init_strats():
+    arr = [
+  PigVIStrat(ENVIRON[0],
+              ChineseRProcess(ENVIRON[0], THETA, ALPHA),
+              COLORS['green'], PLUS=0, EXPLORER=False),
+
+  PigVIStrat(ENVIRON[0],
+              ChineseRProcess(ENVIRON[0], THETA, ALPHA, False, True),
+              COLORS['purple'], PLUS=0, EXPLORER=False),
+          ]
+
+    return arr

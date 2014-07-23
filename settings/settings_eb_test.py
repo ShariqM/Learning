5dbb2279772d93a1073b5faa6f6c3be834a73ea8
Shariq Mobin <Shariq.Mobin@gmail.com>
2014-07-23 13:30:13 -0700

    eb test, compare with optimized

--- settings/settings_eb_test.py
+++ settings/settings_eb_test.py
@@ -20,11 +20,17 @@ MAZE    = 'maze_complex_s10b.mz'    # See files in maze_files/ dir
 STEPS   = 3000                      # Number of time steps to run
 RUNS    = 200                       # Number of runs
 
+COMPLEX_ALPHA = 0.61 # Found empirically
+
 def init_strats():
     arr = [
   PigVIStrat(ENVIRON[0],
-              ChineseRProcess(ENVIRON[0], THETA, ALPHA),
+              ChineseRProcess(ENVIRON[0], THETA, COMPLEX_ALPHA),
               COLORS['green'], PLUS=0, EXPLORER=False),
+
+  PigVIStrat(ENVIRON[0],
+              ChineseRProcess(ENVIRON[0], THETA, ALPHA, False, True, config.FNEW_KL),
+              COLORS['red'], PLUS=0, EXPLORER=False),
 
   PigVIStrat(ENVIRON[0],
               ChineseRProcess(ENVIRON[0], THETA, ALPHA, False, True),
